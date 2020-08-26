b5203932cedf0f433af56868581882d0526e15c6
sato chiharu <kujira028@gmail.com>
2020-08-26 01:39:58 +0000

    定数について

--- dotinstall_1/js/main.js
+++ dotinstall_1/js/main.js
@@ -1,10 +1,11 @@
 'use strict';
 
-console.log(10 + 3);
-console.log(10 - 3);
-console.log(10 * 3);
-console.log(10 / 3);
-console.log(10 % 3);
-console.log(10 ** 3);
+// 定数
 
-console.log((2 + 10) * 3);
+console.log(150 * 140);
+console.log(150 * 140);
+
+const price = 150;  
+
+console.log(price * 140);
+console.log(price * 160);
