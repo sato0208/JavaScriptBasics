67ad50587760b4543ba9f754dc56ad49042eb837
sato chiharu <kujira028@gmail.com>
2020-08-26 01:36:57 +0000

    数値の計算

--- dotinstall_1/js/main.js
+++ dotinstall_1/js/main.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log('Hello World');
+console.log(10 + 3);
+console.log(10 - 3);
+console.log(10 * 3);
+console.log(10 / 3);
+console.log(10 % 3);
+console.log(10 ** 3);
+
+console.log((2 + 10) * 3);
